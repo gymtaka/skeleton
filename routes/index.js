af673760f6b26b4c7fe4e28d9acc07a51b5203a0
Takayuki Igaki <gymtaka@gmail.com>
2017-04-15 19:38:03 +0900

    とりあえず404エラーは出ない状態

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,20 @@
-var express = require('express');
-var router = express.Router();
-var http = require('http').Server(express);
-var io = require('socket.io').listen(http);
+var express = require('express')
+  , http = require('http');
+var app = express();
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
 
-router.get('/', function(req, res){
+
+app.get('/', function(req, res){
   res.render('index',
   { title: 'socket.io'  }
 );
   });
-
+/*
   io.on('connection', function(socket){
   console.log('a user connected');
   });
-
+*/
 /*  var count = 0;
   var countup = function(){
     console.log(count++);
@@ -20,11 +22,11 @@ router.get('/', function(req, res){
   setInterval(countup, 1000);
 */
 io.on('connection', function(socket){
+  console.log('いきてますか');
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
   });
 });
 
 
-
-module.exports = router;
+module.exports = app;
