210057c2c5ae070c40b08be2e3b9ae2b404075ef
Takayuki Igaki <gymtaka@gmail.com>
2017-04-15 15:49:45 +0900

    送信はできるけど受信はできない

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var http = require('http').Server(express);
+var io = require('socket.io').listen(http);
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res){
+  res.render('index',
+  { title: 'socket.io'  }
+);
+  });
+
+  var io = require('socket.io').listen(http);
+  io.on('connection', function(socket){
+  console.log('a user connected');
+  });
+
+/*  io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+      console.log('message: ' + msg);
+    });
+  });
+*/
+/*  var count = 0;
+  var countup = function(){
+    console.log(count++);
+  }
+  setInterval(countup, 1000);
+*/
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
 });
+
 
 module.exports = router;
